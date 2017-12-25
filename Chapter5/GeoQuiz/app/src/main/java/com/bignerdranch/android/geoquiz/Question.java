412cf114b165f175a7431bd815019f39151988a4
Wei Wei <windmaple@gmail.com>
2017-12-25 21:00:03 +0800

    Finished chapter 5

---
+++ Chapter5/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by weiwe on 12/25/17.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+}
