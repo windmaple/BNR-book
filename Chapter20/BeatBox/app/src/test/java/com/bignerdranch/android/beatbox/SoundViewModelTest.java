ee67c71d7edfe58838eae0b1a8fd8fa16f72a1ab
Wei Wei <windmaple@gmail.com>
2018-01-02 17:02:24 +0800

    Finished chapter 20

---
+++ Chapter20/BeatBox/app/src/test/java/com/bignerdranch/android/beatbox/SoundViewModelTest.java
@@ -0,0 +1,38 @@
+package com.bignerdranch.android.beatbox;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by weiwe on 1/2/18.
+ */
+public class SoundViewModelTest {
+    private BeatBox mBeatBox;
+    private Sound mSound;
+    private SoundViewModel mSubject;
+
+    @Before
+    public void setUp() throws Exception {
+        mBeatBox = mock(BeatBox.class);
+        mSound = new Sound("assetPath");
+        mSubject = new SoundViewModel(mBeatBox);
+        mSubject.setSound(mSound);
+    }
+
+    @Test
+    public void exposesSoundNameAsTitle() {
+        assertThat(mSubject.getTitle(), is(mSound.getName()));
+    }
+
+    @Test
+    public void callBeatBoxPlayOnButtonClicked() {
+        mSubject.onButtonClicked();
+
+        verify(mBeatBox).play(mSound);
+    }
+}
