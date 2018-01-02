93a9c3a28741d671cc5ae2282036ac9506589509
Wei Wei <windmaple@gmail.com>
2018-01-02 21:51:41 +0800

    Finished chapter 22

---
+++ Chapter22/BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.java
@@ -0,0 +1,35 @@
+package com.bignerdranch.android.beatbox;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+/**
+ * Created by weiwe on 1/2/18.
+ */
+
+public class SoundViewModel extends BaseObservable {
+    private Sound mSound;
+    private BeatBox mBeatBox;
+
+    public SoundViewModel(BeatBox beatBox) {
+        mBeatBox = beatBox;
+    }
+
+    @Bindable
+    public String getTitle() {
+        return mSound.getName();
+    }
+
+    public Sound getSound() {
+        return mSound;
+    }
+
+    public void setSound(Sound sound) {
+        mSound = sound;
+        notifyChange();
+    }
+
+    public void onButtonClicked() {
+        mBeatBox.play(mSound);
+    }
+}
