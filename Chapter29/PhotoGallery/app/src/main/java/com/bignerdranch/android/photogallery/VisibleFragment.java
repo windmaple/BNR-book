7f8d7f0ee6e5e1648a80e1575387f475ac7d89fc
Wei Wei <windmaple@gmail.com>
2018-01-12 19:12:53 +0800

    Finished chapter 29

---
+++ Chapter29/PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/VisibleFragment.java
@@ -0,0 +1,40 @@
+package com.bignerdranch.android.photogallery;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.ActivityInfo;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by weiwe on 1/12/18.
+ */
+
+public abstract class VisibleFragment extends Fragment {
+    private static final String TAG = "VisisbleFragment";
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        IntentFilter filter = new IntentFilter(PollService.ACTION_SHOW_NOTIFICATION);
+        getActivity().registerReceiver(mOnShowNotification, filter, PollService.PERM_PRIVATE, null);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getActivity().unregisterReceiver(mOnShowNotification);
+    }
+
+    private BroadcastReceiver mOnShowNotification = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i(TAG, "cancelling notification");
+            setResultCode(Activity.RESULT_CANCELED);
+        }
+    };
+}
