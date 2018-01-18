e3e25529773abdf93b6698798447bab56d8e5809
Wei Wei <windmaple@gmail.com>
2018-01-18 21:36:54 +0800

    Finished chapter 31

---
+++ Chapter31/app/src/main/java/com/bignerdranch/android/draganddraw/Box.java
@@ -0,0 +1,29 @@
+package com.bignerdranch.android.draganddraw;
+
+import android.graphics.PointF;
+
+/**
+ * Created by weiwe on 1/18/18.
+ */
+
+public class Box {
+    private PointF mOrigin;
+    private PointF mCurrent;
+
+    public Box(PointF origin) {
+        mOrigin = origin;
+        mCurrent = origin;
+    }
+
+    public PointF getCurrent() {
+        return mCurrent;
+    }
+
+    public void setCurrent(PointF current) {
+        mCurrent = current;
+    }
+
+    public PointF getOrigin() {
+        return mOrigin;
+    }
+}
